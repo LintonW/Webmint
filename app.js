9e8888e1f97d8dfa69e9e1e3f21947b2d9099998
Linton <linton.warren01@gmail.com>
2022-07-13 15:14:29 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const primaryNav = document.querySelector('.primary-nav');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+    const visibilty = primaryNav.getAttribute('data-visible');
+
+    if (visibilty === 'false') {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else if (visibilty === 'true') {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+});
